f6cc48affb3cad00791924050961de5f8a59df38
jbfp <jakobp10@gmail.com>
2015-10-17 16:36:29 +0200

    Removes whitespace

--- src/EmptyWeb/FSharpEmptyWeb/Startup.fs
+++ src/EmptyWeb/FSharpEmptyWeb/Startup.fs
@@ -11,9 +11,9 @@ open Microsoft.Extensions.DependencyInjection
 type Startup () =
     member x.ConfigureServices(services: IServiceCollection) =
         ()
-        
+
     member x.Configure(app: IApplicationBuilder) =
         // Add the platform handler to the request pipeline.
         app.UseIISPlatformHandler() |> ignore
-    
+
         app.Run(fun (context: HttpContext) -> context.Response.WriteAsync("Hello World!"))
